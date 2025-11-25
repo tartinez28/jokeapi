b0d2e56c6722e4c6ee89dcf81695fbcc83ce7a8b
tartinez28 <miguel.martinezt@uniagustiniana.edu.co>
2025-11-25 17:53:11 +0000

    primer cambio

---
+++ proyecto/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBfmMkb8DPXxrK0wKLePgd-MCTyBiF2_7U",
+  authDomain: "jokeapi-3ecd4.firebaseapp.com",
+  projectId: "jokeapi-3ecd4",
+  storageBucket: "jokeapi-3ecd4.firebasestorage.app",
+  messagingSenderId: "1035458021422",
+  appId: "1:1035458021422:web:a9873443b4339681e8b7bf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ proyecto/src/main.js
@@ -0,0 +1,3 @@
+import mostrarRegistro from './componentes/registro.js';
+
+mostrarRegistro()
